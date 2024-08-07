5f54bc44b9d0b22961c9c1995c0165db5598e739
wngud1 <dlqns928@naver.com>
2024-08-07 19:46:10 +0900

    프로젝트 생성

---
+++ ScProject/src/main/java/com/sc/ScProject/ScProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sc.ScProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScProjectApplication.class, args);
+	}
+
+}
